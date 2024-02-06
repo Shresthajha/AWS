8cd5b1dccecfa7eb0b4d8c5435223bbbc77b7d52
Shresthajha <71933929+Shresthajha@users.noreply.github.com>
2024-02-06 17:07:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "ec2Instance" {
   depends_on    = [aws_vpc.dev-vpc]
   ami           = "ami-0d63de463e6604d0a"
   instance_type = "t2.micro"
-  subnet_id     = ["subnet-06ce87a16f747f295", "subnet-0d4eae7125270e8ae"]
+  #subnet_id     = ["subnet-06ce87a16f747f295", "subnet-0d4eae7125270e8ae"]
   
   tags = {
     Name = "my first instance created through terraform"
