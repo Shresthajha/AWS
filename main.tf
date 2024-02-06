e41aeedba6fbeffcfa111cb9c798452ad60fb184
Shresthajha <71933929+Shresthajha@users.noreply.github.com>
2024-02-06 17:25:10 +0530

    Update main.tf
    added security group

--- main.tf
+++ main.tf
@@ -6,9 +6,10 @@ resource "aws_instance" "ec2Instance" {
   ami           = "ami-0d63de463e6604d0a"
   instance_type = "t2.micro"
   subnet_id     = "subnet-06ce87a16f747f295"
+  vpc_security_group_ids = [aws_security_group.dev-sg.id]
   
   tags = {
-    Name = "my first instance created through terraform"
+    Name = "first instance via terraform"
   }
 }
 
@@ -25,5 +26,23 @@ resource "aws_vpc" "dev-vpc" {
 
   #SG
 
-  
+  resource "aws_security_group" "dev-sg" {
+  name        = "dev-sg"
+  description = "security group for allowing all inbound and outbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
  
