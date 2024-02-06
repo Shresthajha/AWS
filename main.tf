ddc44aefaf75d004df7f721f82f757c57e35ee55
Shresthajha <71933929+Shresthajha@users.noreply.github.com>
2024-02-06 17:57:37 +0530

    Update main.tf
    added vpc to sg

--- main.tf
+++ main.tf
@@ -29,6 +29,7 @@ resource "aws_vpc" "dev-vpc" {
   resource "aws_security_group" "dev-sg" {
   name        = "dev-sg"
   description = "security group for allowing all inbound and outbound traffic"
+  vpc_id      = "aws_vpc.dev-vpc"
 
   ingress {
     from_port   = 0
